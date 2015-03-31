ca1ff86d5c44eb8f29c5b0256305888905f88817
inyoungflork <ikwon01@risd.edu>
2015-03-31 16:59:08 -0400

    addjava

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$('svg').ready(function(){
+    
+  $(".map1").click(function(){
+     window.location="http://codepen.io/inyoungkwon/pen/RNOPrP";
+ 	});
+  
+});
+
+/
+$("a#thing_to_click").click(function(){
+         window.location = "http://www.google.com/";    
+});
+
+$('#something').click(function() { 
+    document.location = 'http://somewhere.com/';
+} );
+*/
